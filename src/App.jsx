1e95d48cb984a514983519bd796cb1d7ca7adaf8
Ankit Parihar <ankitparihar844@gmail.com>
2025-11-04 00:10:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './card'
+
+function App() {
+  
+
+  return (
+    <>
+    <div id='cards'>
+         <Card/>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,34 @@
+import "./card.css";
+import {Bookmark} from 'lucide-react'
+
+export default function Card(props) {
+    console.log(props);
+  return (
+    <>
+        <div className="card">
+         <div>
+            <div className="top">
+            <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012015/amazon-logo-rgb.png?itok=FZJ5-sKg" alt="" />
+            <button> Save <Bookmark size={12} /></button>
+         </div>
+         <div className="center">
+            <h3>Amazone <span>5 days ago</span></h3>
+            <h2>Seniour  UI/UX Designer</h2>
+            <div className="tag">
+                <h4>part time</h4>
+                <h4>senior Level</h4>
+            </div>
+         </div>
+         </div>
+         <div className="bottom">
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+         </div>
+        </div>
+    </>
+  );
+}
+
