119e146730c054285d9314b5cdcbe9a30f0768d9
Ankit Parihar <ankitparihar844@gmail.com>
2025-11-05 00:15:16 +0530

    data passing using props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,108 @@ import Card from './card'
 
 function App() {
 
+  const jobListings = [
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg",
+    companyName: "Google",
+    postedDate: "2 days ago",
+    rolePosition: "Senior Frontend Developer",
+    jobType: "Full Time",
+    salary: "$120/hr",
+    location: "Mumbai, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    companyName: "Amazon",
+    postedDate: "5 days ago",
+    rolePosition: "UI/UX Designer",
+    jobType: "Full Time",
+    salary: "$95/hr",
+    location: "Bengaluru, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+    companyName: "Meta",
+    postedDate: "1 day ago",
+    rolePosition: "Full Stack Developer (MERN)",
+    jobType: "Part Time",
+    salary: "$80/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/4/48/Microsoft_logo_%282012%29.svg",
+    companyName: "Microsoft",
+    postedDate: "3 days ago",
+    rolePosition: "Software Engineer",
+    jobType: "Full Time",
+    salary: "$110/hr",
+    location: "Pune, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+    companyName: "Netflix",
+    postedDate: "4 days ago",
+    rolePosition: "Backend Developer (Node.js)",
+    jobType: "Full Time",
+    salary: "$100/hr",
+    location: "Chennai, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Tesla_Motors.svg",
+    companyName: "Tesla",
+    postedDate: "6 days ago",
+    rolePosition: "React Native Developer",
+    jobType: "Part Time",
+    salary: "$90/hr",
+    location: "Delhi, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
+    companyName: "IBM",
+    postedDate: "1 week ago",
+    rolePosition: "Cloud Engineer",
+    jobType: "Full Time",
+    salary: "$105/hr",
+    location: "Noida, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/0/02/LinkedIn_Logo.svg",
+    companyName: "LinkedIn",
+    postedDate: "3 days ago",
+    rolePosition: "Frontend Developer (React)",
+    jobType: "Full Time",
+    salary: "$115/hr",
+    location: "Gurgaon, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/d/db/Adobe_Corporate_Logo.png",
+    companyName: "Adobe",
+    postedDate: "2 days ago",
+    rolePosition: "Senior UI/UX Designer",
+    jobType: "Full Time",
+    salary: "$125/hr",
+    location: "Bengaluru, India"
+  },
+  {
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png",
+    companyName: "Apple",
+    postedDate: "5 days ago",
+    rolePosition: "Frontend Developer (Next.js)",
+    jobType: "Full Time",
+    salary: "$130/hr",
+    location: "Pune, India"
+  }
+];
+
+
   return (
     <>
     <div id='cards'>
-         <Card/>
-         <Card/>
-         <Card/>
+         {
+          jobListings.map(function(elem) {
+            return <Card companyLogo={elem.companyLogo} companyName={elem.companyName} postedDate={elem.postedDate} rolePosition={elem.rolePosition} jobType={elem.jobType} salary={elem.salary} location={elem.location} />
+          })
+         }
     </div>
   
     </>
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,28 +2,29 @@ import "./card.css";
 import {Bookmark} from 'lucide-react'
 
 export default function Card(props) {
-    console.log(props);
+    // console.log(props);
   return ( 
     <>
+    
         <div className="card">
          <div>
             <div className="top">
-            <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012015/amazon-logo-rgb.png?itok=FZJ5-sKg" alt="" />
+            <img src={props.companyLogo} alt="" />
             <button> Save <Bookmark size={12} /></button>
          </div>
          <div className="center">
-            <h3>Amazone <span>/5 days ago</span></h3>
-            <h2>Seniour  UI/UX Designer</h2>
+            <h3>{props.companyName} <span>{props.postedDate}</span></h3>
+            <h2>{props.rolePosition}</h2>
             <div className="tag">
-                <h4>part time</h4>
+                <h4>{props.jobType}</h4>
                 <h4>senior Level</h4>
             </div>
          </div>
          </div>
          <div className="bottom">
             <div className="left">
-                <h3>$120/hr</h3>
-                <p>Mumbai, India</p>
+                <h3>{props.salary}</h3>
+                <p>{props.location}</p>
             </div>
             <button>Apply Now</button>
          </div>
